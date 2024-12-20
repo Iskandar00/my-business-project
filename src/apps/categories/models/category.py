a29d2e939e8603b3c0a0f5b1dc5721787c9ebc11
Muhammadali1620 <muhammadvalievmuhammadali@gmail.com>
2024-12-20 16:43:56 +0500

    Category image

--- src/apps/categories/models/category.py
+++ src/apps/categories/models/category.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='category/images/%Y/%m/%d/')
 
     def __str__(self):
         return self.name
