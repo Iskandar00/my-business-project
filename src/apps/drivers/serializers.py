b6a13717b07e79c423243d38ce32c1ee33aa69a1
Iskandar <diyoroviskandar00@gmail.com>
2025-02-14 15:28:42 +0500

    a

--- src/apps/drivers/serializers.py
+++ src/apps/drivers/serializers.py
@@ -4,7 +4,7 @@ from .models import ProductDelivery
 class ProductDeliverySerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductDelivery
-        fields = ('delivery', 'order', 'status', 'product', 'product_count', 'created_at', 'updated_at',)
+        fields = ('id', 'delivery', 'order', 'status', 'product', 'product_count', 'created_at', 'updated_at',)
         read_only_fields = ('product', 'product_count', 'created_at', 'updated_at')
 
     def validate(self, attrs):
